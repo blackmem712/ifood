26dda7314ca1c1b7b00d23e049efb33068bafef3
ElitonRoberto <elitongt45@gmail.com>
2023-06-24 01:30:47 -0300

    atualização dos restaurantes

--- ifood/lib/src/restaurante.dart
+++ ifood/lib/src/restaurante.dart
@@ -63,9 +63,156 @@ class PageRestaurante extends StatelessWidget {
             ),  
           ),
 
+          /////////////////////////////////////////////
+          ////// 3 restaurante a lista/////////////////
+          //////////////////////////////////////////////
+          
+          GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const HomeDois()),
+              );
+            },
+            child: Container( 
+              height:100,
+              color: Colors.white54,           
+              child: Row(
+                children: [
+                  Image.asset('assets/restaurantes/rest3.png'),
+                  const SizedBox(
+                      width: 8), // Espaçamento entre a imagem e o rótulo
+                  const Text('Coco Bambu'), 
+                ],
+              ),
+            ),  
+          ),
 
 
+          /////////////////////////////////////////////
+          ////// 4 restaurante a lista/////////////////
+          //////////////////////////////////////////////
+          
+          GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const HomeDois()),
+              );
+            },
+            child: Container( 
+              height:100,
+              color: Colors.white54,           
+              child: Row(
+                children: [
+                  Image.asset('assets/restaurantes/rest4.png'),
+                  const SizedBox(
+                      width: 8), // Espaçamento entre a imagem e o rótulo
+                  const Text('Comida Quente'), 
+                ],
+              ),
+            ),  
+          ),
 
+          /////////////////////////////////////////////
+          ////// 5 restaurante a lista/////////////////
+          //////////////////////////////////////////////
+          
+          GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const HomeDois()),
+              );
+            },
+            child: Container( 
+              height:100,
+              color: Colors.white54,           
+              child: Row(
+                children: [
+                  Image.asset('assets/restaurantes/rest5.png'),
+                  const SizedBox(
+                      width: 8), // Espaçamento entre a imagem e o rótulo
+                  const Text('Ribeiro'), 
+                ],
+              ),
+            ),  
+          ),
+
+          /////////////////////////////////////////////
+          ////// 6 restaurante a lista/////////////////
+          //////////////////////////////////////////////
+          
+          GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const HomeDois()),
+              );
+            },
+            child: Container( 
+              height:100,
+              color: Colors.white54,           
+              child: Row(
+                children: [
+                  Image.asset('assets/restaurantes/rest6.png'),
+                  const SizedBox(
+                      width: 8), // Espaçamento entre a imagem e o rótulo
+                  const Text('Villa Gourmet'), 
+                ],
+              ),
+            ),  
+          ),
+
+          /////////////////////////////////////////////
+          ////// 7 restaurante a lista/////////////////
+          //////////////////////////////////////////////
+          
+          GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const HomeDois()),
+              );
+            },
+            child: Container( 
+              height:100,
+              color: Colors.white54,           
+              child: Row(
+                children: [
+                  Image.asset('assets/restaurantes/rest7.png'),
+                  const SizedBox(
+                      width: 8), // Espaçamento entre a imagem e o rótulo
+                  const Text('Catering'), 
+                ],
+              ),
+            ),  
+          ),
+
+          /////////////////////////////////////////////
+          ////// 8 restaurante a lista/////////////////
+          //////////////////////////////////////////////
+          
+          GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const HomeDois()),
+              );
+            },
+            child: Container( 
+              height:100,
+              color: Colors.white54,           
+              child: Row(
+                children: [
+                  Image.asset('assets/restaurantes/rest8.png'),
+                  const SizedBox(
+                      width: 8), // Espaçamento entre a imagem e o rótulo
+                  const Text('Epic Restaurant'), 
+                ],
+              ),
+            ),  
+          ),
 
         ],       
       ),
